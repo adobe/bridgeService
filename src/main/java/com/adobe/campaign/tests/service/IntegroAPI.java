a189f2c3daf35a3ed55f950241245836ba12775d
Baubak Gandomi <gandomi@adobe.com>
2023-03-02 17:24:57 +0100

    first working version

--- src/main/java/com/adobe/campaign/tests/service/IntegroAPI.java
+++ src/main/java/com/adobe/campaign/tests/service/IntegroAPI.java
@@ -18,7 +18,7 @@ public class IntegroAPI {
     public static final String ERROR_JSON_TRANSFORMATION = "JSON Transformation issue : Problem processing request. The given json could not be mapped to a Java Call";
     private static final String ERROR_CALLING_JAVA_METHOD = "Error during call of target Java Class and Method.";
     private static final String ERROR_JAVA_OBJECT_NOT_FOUND = "Could not fid the given class or method.";
-    public static final String SYSTEM_VERSION = "0.0.7";
+    public static final String SYSTEM_VERSION = "0.0.8";
 
     public static void
     startServices(int port) {

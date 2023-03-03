d7dbaac35b7fc78ee4b98db88b167c5b00db91cb
Baubak Gandomi <gandomi@adobe.com>
2023-03-03 10:01:51 +0100

    enriched the E2E tests

--- src/test/java/com/adobe/campaign/tests/service/E2ETests.java
+++ src/test/java/com/adobe/campaign/tests/service/E2ETests.java
@@ -1,6 +1,5 @@
 package com.adobe.campaign.tests.service;
 
-import com.adobe.campaign.tests.integro.tools.RandomManager;
 import org.hamcrest.Matchers;
 import org.testng.annotations.AfterGroups;
 import org.testng.annotations.BeforeGroups;
@@ -21,7 +20,6 @@ public class E2ETests {
         IntegroAPI.startServices(8080);
         Spark.awaitInitialization();
     }
-
 
     @Test(groups = "E2E")
     public void testMainHelloWorld() {
@@ -70,11 +68,9 @@ public class E2ETests {
     @Test(groups = "E2E")
     public void testCheckConnectivity() throws IOException {
 
-
         Map<String, String> urlsMap = new HashMap<>();
         urlsMap.put("url1", "not really a url");
         urlsMap.put("url2", "acc-simulators.email.corp.adobe.com:143");
-
 
         given().body(urlsMap).post(EndPointURL + "service-check").then().assertThat().statusCode(200)
                 .body("url1", Matchers.equalTo(false), "url2", Matchers.equalTo(true));
@@ -83,6 +79,7 @@ public class E2ETests {
 
     /**
      * Testing that we provide the correct error messages whenever the target method throws an error
+     *
      * @throws IOException
      */
     @Test(groups = "E2E")
@@ -99,6 +96,41 @@ public class E2ETests {
 
         given().body(l_call).post(EndPointURL + "call").then().assertThat().statusCode(400).body(
                 Matchers.containsString("Minimum number must be strictly inferior than maximum number."));
+
+    }
+
+    @Test
+    public void testIntegrity() {
+
+        //Call 1
+        JavaCalls l_myJavaCalls = new JavaCalls();
+
+        CallContent l_cc = new CallContent();
+        l_cc.setClassName("com.adobe.campaign.tests.integro.tools.RandomManager");
+        l_cc.setMethodName("getRandomEmail");
+
+        Map<String, Object> l_authentication = new HashMap<>();
+        l_authentication.put("AC.UITEST.MAILING.PREFIX", "bada");
+        l_authentication.put("AC.INTEGRO.MAILING.BASE", "boom.com");
+
+        l_myJavaCalls.setEnvironmentVariables(l_authentication);
+
+        l_myJavaCalls.getCallContent().put("call1PL", l_cc);
+
+        given().body(l_myJavaCalls).post(EndPointURL + "call").then().assertThat().statusCode(200).
+                body("returnValues.call1PL", Matchers.endsWith("@boom.com"))
+                .body("returnValues.call1PL", Matchers.startsWith("bada"));
+
+
+        JavaCalls l_myJavaCallsB = new JavaCalls();
+        CallContent l_ccB = new CallContent();
+        l_ccB.setClassName("com.adobe.campaign.tests.integro.tools.RandomManager");
+        l_ccB.setMethodName("getRandomEmail");
+        l_myJavaCallsB.getCallContent().put("getRandomEmailB", l_ccB);
+
+        given().body(l_myJavaCallsB).post(EndPointURL + "call").then().assertThat().statusCode(200).
+                body("returnValues.call1PL", Matchers.not(Matchers.endsWith("@boom.com")))
+                .body("returnValues.call1PL", Matchers.not(Matchers.startsWith("bada")));
 
     }
 
